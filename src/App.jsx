bf28e5120cc5a7b2f08a7ab73252eecab0f35c99
Leo-Thompson <71647794+Leo-Thompson@users.noreply.github.com>
2025-05-13 14:10:09 +0100

    Revert "Update App.jsx"
    This reverts commit 0ba2d81e5946161c43a740d7d2d0053e186cca60.

--- src/App.jsx
+++ src/App.jsx
@@ -93,7 +93,7 @@ export default function  App()  {
             <View>
               <Heading level="3">{userprofile.email}</Heading>
               
-                <Scheduler id="scheduler" dataSource = {schedulerData}>
+                <Scheduler id="scheduler" dataSource = {schedulerData} ref = {this.schedulerRef}>
                   {/* Configuration goes here */}
                 </Scheduler>
                 
